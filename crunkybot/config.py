e9cfb25bfa084d7a929530e3b255509cce86f8cb
Andrew Smith <amsmith87@gmail.com>
2018-06-01 11:23:03 -0700

    Added login features. Began setting up for Celery.

--- crunkybot/config.py
+++ crunkybot/config.py
@@ -4,11 +4,18 @@
 from urllib import quote_plus as urlquote
 
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cockeyedgaming:Djdesertf0x@crunkybot.cfa3vir9mjyz.us-west-2.rds.amazonaws.com:3306/crunkydb'
-
-# Uncomment the line below if you want to work with a local DB
+# DB parameters.
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-
 SQLALCHEMY_POOL_RECYCLE = 3600
-
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
+SECRET_KEY = 'c7a38fff-9bab-4b8e-b3ca-5cb55ec439a3'
+
+#Twitch parameters.
+TWITCH_CLIENTID="i202c5n9be1v8cppav0fic5gfqe7jm"
+TWITCH_SECRET="blztot2h6y8l5j7bh0ljf6qziqxy81"
+
+#Celery parameters
+SOCKETIO_REDIS_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CSRF_ENABLED = True
