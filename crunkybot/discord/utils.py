bde5e2cd70b0e7d0edcab2793c4fdb79cd401fad
Andrew Smith <amsmith87@gmail.com>
2018-10-14 13:10:30 -0700

    Added discord bot.

---
+++ crunkybot/discord/utils.py
@@ -0,0 +1,27 @@
+import youtube_dl
+import config
+
+
+ydl_info_options={'outtmpl': '%(id)s %(title)s'}
+ydl_download_options={
+	'outtmpl': config.MUSIC_DL_LOCATION+'%(id)s %(title)s.%(ext)s',
+	'format': 'bestaudio/best',
+	'postprocessors': [{
+		'key': 'FFmpegExtractAudio',
+		'preferredcodec': 'mp3',
+		'preferredquality': '192'
+	}]
+}
+
+def get_vid_info(vid):
+    with youtube_dl.YoutubeDL(ydl_info_options) as ydl:
+        result=ydl.extract_info(vid,download=False)
+        id_tuple=ydl.prepare_filename(result)
+        vidid=id_tuple.split()[0]
+        title=" ".join(id_tuple.split()[1:])
+        return (vidid,title,result)
+    return False
+
+def download_vid(vid):
+	with youtube_dl.YoutubeDL(ydl_download_options) as ydl:
+		return ydl.download([vid])
